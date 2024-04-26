dbae664d09cde091740098e0ba9bcb71bdc29bb4
Bach Tran Nhat Linh <linhbtn@unitech.net>
2024-04-26 17:07:04 +0700

    add library scss to project

--- src/components/users/users.table.tsx
+++ src/components/users/users.table.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import type { TableProps } from 'antd';
 
 import '~/styles/users.css'
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import UsersPage from './screens/users.page.tsx'
+
+import { FireOutlined, TeamOutlined } from '@ant-design/icons'
+import type { MenuProps } from 'antd'
+import { Menu } from 'antd'
+
 import './index.css'
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, Link, Outlet, RouterProvider } from 'react-router-dom'
+
+const items: MenuProps['items'] = [
+  {
+    label: <Link to={'/'}>Home</Link>,
+    key: 'home',
+    icon: <FireOutlined />
+  },
+  {
+    label: <Link to={'/users'}>Manage Users</Link>,
+    key: 'users',
+    icon: <TeamOutlined />,
+  },
+]
+
+
+const Header = () => {
+  const [current, setCurrent] = useState('home')
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    console.log('click ', e)
+    setCurrent(e.key)
+  }
+
+  return <Menu onClick={onClick} selectedKeys={[current]} mode='horizontal' items={items} />
+}
+
+const LayoutAdmin = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <footer></footer>
+    </div>
+  )
+}
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/users",
-    element: <UsersPage />,
-  },
+    path: '/',
+    // element: <App />,
+    element: <LayoutAdmin />,
+    children: [
+      { index: true, element: <App /> },
+      {
+        path: '/users',
+        element: <UsersPage />
+      }
+    ]
+  }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* <App /> */}
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
